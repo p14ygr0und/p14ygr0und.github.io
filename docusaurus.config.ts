84d5ac1b3989dd45ea5e1c5d1ee1b3905e59936b
Hi120ki <12624257+hi120ki@users.noreply.github.com>
2025-05-10 19:58:32 +0900

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,19 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://p14ygr0und.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  // organizationName: "facebook", // Usually your GitHub org/user name.
+  // projectName: "docusaurus", // Usually your repo name.
+
+  projectName: "p14ygr0und.github.io",
+  organizationName: "p14ygr0und",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
